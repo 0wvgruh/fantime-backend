15c65e206c4863094b5029e5e7d5dc4bb99381b8
0wvgruh <ksh02040778@gmail.com>
2026-06-22 17:43:37 +0900

    initial commit

---
+++ src/main/java/com/fantime/fantime_backend/FantimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fantime.fantime_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantimeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantime/fantime_backend/FantimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantime.fantime_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
